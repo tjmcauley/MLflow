bb9d0a14b11f8f1cf958800444bfbcb770a7b7fd
tjmcauley <tjmcauley1234@gmail.com>
2025-02-02 16:38:39 +0000

    Copied code from MLflow to log the model and its metadata to MLflow

--- tracking_quickstart.py
+++ tracking_quickstart.py
@@ -33,3 +33,32 @@ y_pred = lr.predict(X_test)
 
 # Calculate metrics
 accuracy = accuracy_score(y_test, y_pred)
+
+# Set our tracking server uri for logging
+mlflow.set_tracking_uri(uri="http://127.0.0.1:8080")
+
+# Create a new MLflow Experiment
+mlflow.set_experiment("MLflow Quickstart")
+
+# Start an MLflow run
+with mlflow.start_run():
+    # Log the hyperparameters
+    mlflow.log_params(params)
+
+    # Log the loss metric
+    mlflow.log_metric("accuracy", accuracy)
+
+    # Set a tag that we can use to remind ourselves what this run was for
+    mlflow.set_tag("Training Info", "Basic LR model for iris data")
+
+    # Infer the model signature
+    signature = infer_signature(X_train, lr.predict(X_train))
+
+    # Log the model
+    model_info = mlflow.sklearn.log_model(
+        sk_model=lr,
+        artifact_path="iris_model",
+        signature=signature,
+        input_example=X_train,
+        registered_model_name="tracking-quickstart",
+    )
